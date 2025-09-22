f1e1b6cfe7565c9d85409fe6e8574a4145d6ae7c
CalebeHogrefe <calebe_hogrefe@estudante.sesisenai.org.br>
2025-09-22 08:48:10 -0300

    php, validação

--- src/paginas/cadastro/cadastrar.php
+++ src/paginas/cadastro/cadastrar.php
@@ -2,25 +2,15 @@
 SESSION_START();
 include('database.php');
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-     <form method="POST" action="">
-        <label for="nome">Nome:</label>
-        <input type="text" id="nome" name="nome" required>
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
 
-        <label for="email">E-mail:</label>
-        <input type="email" id="email" name="email" required>
+    
+}
 
-        <label for="senha">Senha:</label>
-        <input type="password" id="senha" name="senha" required>
 
-        <button type="submit" name=""></button>
 
-</body>
-</html>
+
+?>
