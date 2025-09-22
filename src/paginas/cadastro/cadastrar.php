1b399911a2ff8b8df59ca608aee47e529542fb44
drey31008 <andrey_f_santos@estudante.sesisenai.org.br>
2025-09-22 08:49:00 -0300

    Validação de e-mail

--- src/paginas/cadastro/cadastrar.php
+++ src/paginas/cadastro/cadastrar.php
@@ -1,26 +1,16 @@
 <?PHP
 SESSION_START();
 include('database.php');
+
+if(isset($_POST['Validação de e-mail para garantir que esteja em formato correto'])){
+
+   $email = $_POST['email'];
+
+   if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+       // E-mail válido
+   } else {
+       // E-mail inválido
+   }
+
+}
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-     <form method="POST" action="">
-        <label for="nome">Nome:</label>
-        <input type="text" id="nome" name="nome" required>
-
-        <label for="email">E-mail:</label>
-        <input type="email" id="email" name="email" required>
-
-        <label for="senha">Senha:</label>
-        <input type="password" id="senha" name="senha" required>
-
-        <button type="submit" name=""></button>
-
-</body>
-</html>
