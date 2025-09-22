f40fd82b355d238466c79625623e4c07b51005e6
drey31008 <andrey_f_santos@estudante.sesisenai.org.br>
2025-09-22 08:39:39 -0300

    php

---
+++ src/paginas/cadastro/cadastrar.php
@@ -0,0 +1,26 @@
+<?PHP
+SESSION_START();
+include('database.php');
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+     <form method="POST" action="">
+        <label for="nome">Nome:</label>
+        <input type="text" id="nome" name="nome" required>
+
+        <label for="email">E-mail:</label>
+        <input type="email" id="email" name="email" required>
+
+        <label for="senha">Senha:</label>
+        <input type="password" id="senha" name="senha" required>
+
+        <button type="submit" name=""></button>
+
+</body>
+</html>
