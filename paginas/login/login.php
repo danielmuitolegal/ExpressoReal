d0beeee355a50cad653019cfdb8f19b934134006
danielmuitolegal <daniel_schutz@estudante.sesisenai.org.br>
2025-10-06 08:38:36 -0300

    comitado

--- paginas/login/login.php
+++ paginas/login/login.php
@@ -68,7 +68,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <title>Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-    <script src="cadastro.js"></script>
     <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 </head>
 
