7a65b6b2fb3500a2af954c32fa08507ba96bb44d
CalebeHogrefe <calebe_hogrefe@estudante.sesisenai.org.br>
2025-10-29 09:27:13 -0300

    wawd

--- paginas/dashboard/dashboard.php
+++ paginas/dashboard/dashboard.php
@@ -67,54 +67,60 @@ include '../dashboard/dropdown.php';
 
 <body>
   <!-- NAVBAR -->
- <nav class="navbar navbar-expand-lg navbar-light bg-light shadow">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light shadow">
     <div class="container-fluid">
-        <a class="navbar-brand" href="../dashboard/dashboard.php">
-            <img src="../../imagens/logo.png" alt="logo" width="38" height="30" loading="lazy">
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+      <a class="navbar-brand" href="../dashboard/dashboard.php">
+        <img src="../../imagens/logo.png" alt="logo" width="38" height="30" loading="lazy">
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item mx-2"><a class="nav-link active" href="../dashboard/dashboard.php">Home</a></li>
-                <li class="nav-item mx-2"><a class="nav-link" href="../itinerários/itinerários.php">Trens/Rotas</a></li>
-                <li class="nav-item mx-2"><a class="nav-link" href="../manutenção/manutencao.php">Manutenção</a></li>
-            </ul>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item mx-2"><a class="nav-link active" href="../dashboard/dashboard.php">Home</a></li>
+          <li class="nav-item mx-2"><a class="nav-link active" href="../itinerários/itinerários.php">Trens/Rotas</a></li>
+          <li class="nav-item mx-2"><a class="nav-link active" href="../manutenção/manutencao.php">Manutenção</a></li>
+        </ul>
 
-            <form class="d-flex ms-3 me-3 my-2" role="search"> 
-                <input class="form-control me-2" type="search" placeholder="Pesquisar" aria-label="Search">
-                <button class="btn btn-outline-dark" type="submit">Buscar</button>
-            </form>
+        <form class="d-flex ms-3 me-3 my-2" role="search">
+          <input class="form-control me-2" type="search" placeholder="Pesquisar" aria-label="Search">
+          <button class="btn btn-outline-dark" type="submit">Buscar</button>
+        </form>
 
-            <ul class="nav nav-pills ms-3">
-                
-                <li class="nav-item dropdown">
-                    <a class="nav-link bg-primary text-white position-relative"
-                       href="#"
-                       id="notificacoesDropdown"
-                       role="button"
-                       data-bs-toggle="dropdown"
-                       aria-expanded="false">
-                        <img src="https://www.svgrepo.com/show/431413/alert.svg" alt="alerta" width="22">
+        <ul class="nav nav-pills ms-3 d-flex align-items-center">
 
-                        <?php if (isset($_SESSION['notificacoes_count']) && $_SESSION['notificacoes_count'] > 0) : ?>
-                            <span class="notification-badge position-absolute translate-middle badge rounded-circle bg-danger">
-                                <?php echo $_SESSION['notificacoes_count']; ?>
-                            </span>
-                        <?php endif; ?>
-                    </a>
-                    
-                    <?php 
-                        // Note: O dropdown.php não pode ter o require_once 'database.php'; nem o $conn->close();
-                        include '../dashboard/dropdown.php'; 
-                    ?>
-                </li>
-            </ul>
-        </div>
+          <li class="nav-item dropdown me-3 d-flex align-items-center">
+            <a class="nav-link bg-primary text-white position-relative"
+              href="#"
+              id="notificacoesDropdown"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false">
+              <img src="https://www.svgrepo.com/show/431413/alert.svg" alt="alerta" width="22">
+
+
+              <?php if (isset($_SESSION['notificacoes_count']) && $_SESSION['notificacoes_count'] > 0) : ?>
+                <span class="notification-badge position-absolute translate-middle badge rounded-circle bg-danger">
+                  <?php echo $_SESSION['notificacoes_count']; ?>
+                </span>
+              <?php endif; ?>
+            </a>
+
+
+            <?php
+            // Note: O dropdown.php não pode ter o require_once 'database.php'; nem o $conn->close();
+            include '../dashboard/dropdown.php';
+            ?>
+          </li>
+          <div class="d-flex align-items-center">
+            <span class="navbar-text me-3">Olá, <?php echo $nome; ?>!</span>
+            <a href="dashboard/dashbord.php" class="btn btn-outline-dark btn-sm">Sair</a>
+          </div>
+        </ul>
+      </div>
     </div>
-</nav>
+  </nav>
 
   <!-- CONTEÚDO PRINCIPAL -->
   <div class="container mt-4">
@@ -149,7 +155,7 @@ include '../dashboard/dropdown.php';
   </div>
 
   <footer>
-        
+
   </footer>
 </body>
 
